1f76110e2b3676f78651c291a1d2f8221ea0caf4
Huy Trần <quanghuytxc@gmail.com>
2024-12-09 01:35:04 +0700

    fix

--- View/View_Main/FormLoginSignup.Designer.cs
+++ View/View_Main/FormLoginSignup.Designer.cs
@@ -69,8 +69,10 @@
             // 
             // pictureBox1
             // 
-  
-            this.pictureBox1.Location = new System.Drawing.Point(57, 94);
+
+            //this.pictureBox1.Image = global::Quan_ly_thu_vien_phim.Properties.Resources.Logo__3_;
+            this.pictureBox1.Location = new System.Drawing.Point(60, 61);
+
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(191, 158);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
