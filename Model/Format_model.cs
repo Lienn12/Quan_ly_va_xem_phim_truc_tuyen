b6834fb370e0203d31307ddba1a2716bc8f3e47c
Lienn12 <liennguyen4221@gmail.com>
2024-12-09 13:33:17 +0700

    them movie

---
+++ Model/Format_model.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quan_ly_thu_vien_phim.Model
+{
+    public class Format_model
+    {
+        public int FormatID {  get; set; }
+        public string FormatName { get; set; }
+        public Format_model() { }
+        public Format_model(int formatID, string formatName) 
+        {
+            FormatID = formatID;
+            FormatName= formatName;
+        }
+    }
+}
