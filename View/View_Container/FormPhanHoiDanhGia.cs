6260177a6cf3ef7d2270a4f9800b5e779563d35e
Huy Trần <quanghuytxc@gmail.com>
2024-12-09 15:29:27 +0700

    update

--- View/View_Container/FormPhanHoiDanhGia.cs
+++ View/View_Container/FormPhanHoiDanhGia.cs
@@ -12,10 +12,22 @@ namespace Quan_ly_thu_vien_phim.View.View_Container
 {
     public partial class FormPhanHoiDanhGia : Form
     {
-        public FormPhanHoiDanhGia()
+        private int reviewId; // luu thong tin id danh gia duoc truyen vao 
+        public FormPhanHoiDanhGia(int reviewId)
         {
             InitializeComponent();
+            this.reviewId = reviewId; // luuu gia tri truyn vao
         }
 
+        private void btnTru_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormPhanHoiDanhGia_Load(object sender, EventArgs e)
+        {
+            // Sử dụng reviewId để hiển thị thông tin hoặc thực hiện logic cần thiết
+            MessageBox.Show($"ID đánh giá đang xem: {reviewId}", "Thông Tin");
+        }
     }
 }
