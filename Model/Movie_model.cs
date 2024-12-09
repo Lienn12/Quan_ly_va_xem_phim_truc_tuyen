35af795a1e5e77d827ddd0949c996f2f6b5fb942
Lienn12 <liennguyen4221@gmail.com>
2024-12-09 13:25:15 +0700

    update

--- Model/Movie_model.cs
+++ Model/Movie_model.cs
@@ -8,20 +8,19 @@ namespace Quan_ly_thu_vien_phim.Model
 {
     public class Movie_model
     {
-        public int Id { get; set; } // ID duy nhất của phim
-        public string Title { get; set; } // Tiêu đề phim
-        public int Year { get; set; } // Năm sản xuất
-        public string Director { get; set; } // Đạo diễn
-        public string Cast { get; set; } // Dàn diễn viên (có thể là một chuỗi phân cách bởi dấu phẩy)
-        public string Type { get; set; } // Thể loại phim (vd: Action, Drama, ...)
-        public string Format { get; set; } // Định dạng phim (vd: HD, 4K, ...)
-        public string Country { get; set; } // Quốc gia sản xuất
-        public string Description { get; set; } // Mô tả phim
-        public int Episode { get; set; } // Số tập phim (đối với series, nếu không thì mặc định là 1)
-        public string ImgPath { get; set; } // Ảnh đại diện lưu dưới dạng mảng byte
-        public string VidPath { get; set; } // Đường dẫn video
-
-        // Constructor mặc định
+        public int Id { get; set; } 
+        public string Title { get; set; } 
+        public int Year { get; set; }
+        public string Director { get; set; } 
+        public string Cast { get; set; }
+        public string Type { get; set; } 
+        public string Format { get; set; } 
+        public string Country { get; set; } 
+        public string Description { get; set; }
+        public int Episode { get; set; } 
+        public string ImgPath { get; set; }
+        public string VidPath { get; set; } 
+        
         public Movie_model()
         {
         }
@@ -42,7 +41,7 @@ namespace Quan_ly_thu_vien_phim.Model
             this.VidPath = vidPath;
         }
 
-        // Override phương thức ToString() để dễ dàng hiển thị thông tin phim (không hiển thị ảnh vì là nhị phân)
+     
         public override string ToString()
         {
             return $"ID: {Id}, Title: {Title}, Year: {Year}, Director: {Director}, Cast: {Cast}, Type: {Type}, Format: {Format}, Country: {Country}, Description: {Description}, Episode: {Episode},ImgPath: {ImgPath}, VidPath: {VidPath}";
